23408ff6b7ba7bf859202ae271a5705071ebac65
zealantanner <zealantanner@gmail.com>
2021-09-21 20:46:57 -0600

    1

---
+++ app.js
@@ -0,0 +1,12 @@
+$(".navItem").each(function(){
+    $(this).css("color","white");
+});
+
+$("#nav").css("background-color","#145");
+
+$("li:even").css("background-color","#ccc");
+
+$("li:odd").css("background-color","#aaa");
+
+$("label").text("Zealan Tanner");
+$("input").attr("placeholder", "Zealan Tanner");
